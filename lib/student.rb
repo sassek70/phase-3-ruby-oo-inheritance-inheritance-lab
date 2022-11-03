ef281f5bfa113de5ad4433ebf51f3157677bc6c8
Kevin Sasse <sassek70@gmail.com>
2022-11-03 10:03:54 -0400

    finished lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(brain_boost)
+        @knowledge << brain_boost
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
